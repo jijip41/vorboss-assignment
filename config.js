c70b4770886ed8d28072fe1c17f15fa0239267b5
jihye <jiji.pak41@gmail.com>
2022-12-13 15:48:46 +0000

    Set config

---
+++ config.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (!value) throw new Error(`${key} not found`);
+  return value;
+}
+
+export const config = {
+  airtable: {
+    endpontUrl: required('AIRTABLE_ENDPOINT_URL'),
+    apiKey: required('AIRTABLE_API_KEY'),
+    base: required('AIRTABLE_BASE'),
+  },
+};
