0c33e84b730001a0d1c1fe11cb2f5aa2299709f4
jihye <jiji.pak41@gmail.com>
2022-12-13 15:50:11 +0000

    Set server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const path = require('path');
-const express = require('express');
+import express from 'express';
+import Airtable from 'airtable';
+import cors from 'cors';
+import { config } from '../config.js';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+Airtable.configure({
+  endpointUrl: config.airtable.endpontUrl,
+  apiKey: config.airtable.apiKey,
+});
+const base = Airtable.base(config.airtable.base);
+
+app.use(cors());
+
+app.get('/getAll', (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
