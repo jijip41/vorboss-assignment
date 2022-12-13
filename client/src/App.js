b30e29965d89bf364896c2279a5926e46e9065a0
jihye <jiji.pak41@gmail.com>
2022-12-13 20:08:55 +0000

    Create QueryClientProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
+import Home from './Home.js';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
