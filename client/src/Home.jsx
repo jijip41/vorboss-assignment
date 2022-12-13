a5a439c4112830f889ea4a23f998288a9f27c95e
jihye <jiji.pak41@gmail.com>
2022-12-13 22:35:38 +0000

    Show recent 10 orders

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -35,8 +35,29 @@ export default function Home() {
             Total Orders this month:
             {sortOrdersByMonth(orders, monthToday).length}
           </p>
+          <p>
+            Orders in progress:{' '}
+            {getOrdersByStatus(orders, 'in_progress').length}
+          </p>
           <p> Revenue: {getTotalRevenue(orders)}</p>
-          <p> Recent orders</p>
+          <p>
+            {' '}
+            Recent orders:{' '}
+            <div className="flex">
+              <div>Order number</div>
+              <div>Date</div>
+              <div>Product Name</div>
+              <div>Order Status</div>
+            </div>
+            {sortOrdersByDate(orders, 10).map((order) => (
+              <div key={order.order_id} className="flex">
+                <p>{order.order_id}</p>
+                <p>{order.order_placed}</p>
+                <p>{order.product_name}</p>
+                <p>{order.order_status}</p>
+              </div>
+            ))}
+          </p>
         </div>
       )}
     </main>
@@ -52,4 +73,15 @@ function sortOrdersByMonth(orders, month) {
 function getTotalRevenue(orders) {
   const total = orders.reduce((a, c) => a + c.price, 0);
   return total.toFixed(2);
+}
+
+function getOrdersByStatus(orders, status) {
+  return orders.filter((order) => order.order_status === status);
+}
+
+function sortOrdersByDate(orders, number) {
+  const recentOrders = orders.sort(
+    (a, b) => new Date(b.date) - new Date(a.date)
+  );
+  return recentOrders.slice(0, number);
 }
