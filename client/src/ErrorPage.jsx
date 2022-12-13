e51cf5a7dcea2748cb056ed96e2c192719846b28
jihye <jiji.pak41@gmail.com>
2022-12-13 20:35:29 +0000

    Create error Page

---
+++ client/src/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CircleWavyWarning } from 'phosphor-react';
+
+export default function ErrorPage(props) {
+  return (
+    <div>
+      <CircleWavyWarning size={32} />
+    </div>
+  );
+}
