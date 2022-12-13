dc99d70df69a95f2b6b01c7acb3b1d55d49a536a
jihye <jiji.pak41@gmail.com>
2022-12-13 16:27:34 +0000

    Create endpoint to get all products

--- server/index.js
+++ server/index.js
@@ -15,7 +15,27 @@ const base = Airtable.base(config.airtable.base);
 
 app.use(cors());
 
-app.get('/getAll', (req, res) => {});
+app.get('/getAll', async (req, res) => {
+  const arr = [];
+  await base
+    .table('Orders')
+    .select({})
+    .eachPage((records, next) => {
+      records.forEach((rec) => {
+        try {
+          arr.push(rec.fields);
+        } catch (err) {
+          console.error(err);
+        }
+      });
+      try {
+        next();
+      } catch {
+        return;
+      }
+    }),
+    res.status(200).send(arr);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
