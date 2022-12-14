ad1a02c7870a80b8f225e0e7c0d9c9b339147bf5
jihye <jiji.pak41@gmail.com>
2022-12-14 12:07:49 +0000

    Style error page

--- client/src/ErrorPage.jsx
+++ client/src/ErrorPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { CircleWavyWarning } from 'phosphor-react';
+import './index.css';
 
 export default function ErrorPage(props) {
   return (
-    <div>
-      <CircleWavyWarning size={32} />
+    <div className="flex-col-center w-h-screen">
+      <CircleWavyWarning size={32} className="text-color-vorboss1" />
+      <p>Sorry, something went wrong</p>
     </div>
   );
 }
