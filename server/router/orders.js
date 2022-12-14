cc2d2a13366b79bfbed7daeb7c492d0dbec7827a
jihye <jiji.pak41@gmail.com>
2022-12-14 20:49:17 +0000

    Add api endpoint

--- server/router/orders.js
+++ server/router/orders.js
@@ -10,10 +10,39 @@ const base = Airtable.base(config.airtable.base);
 
 const router = express.Router();
 
+router.get('/total', async (req, res, next) => {
+  const arr = [];
+  await base
+    .table('Orders')
+    .select({
+      view: 'Grid view',
+      sort: [{ field: config.airtable.placedDateId, direction: 'desc' }],
+    })
+    .eachPage((records, next) => {
+      records.forEach((rec) => {
+        try {
+          arr.push(rec.fields);
+        } catch (err) {
+          console.error(err);
+        }
+      });
+      try {
+        next();
+      } catch {
+        return;
+      }
+    });
+  if (arr) {
+    res.status(200).json(arr);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 router.get('/:maxRecords', async (req, res, next) => {
   const maxNumber = Number(req.params.maxRecords);
   const arr = [];
-  const data = await base
+  await base
     .table('Orders')
     .select({
       maxRecords: maxNumber,
