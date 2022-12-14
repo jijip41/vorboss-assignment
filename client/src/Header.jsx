e1958513d2d091d37292ac5ca1c09d93fca09f0b
jihye <jiji.pak41@gmail.com>
2022-12-14 12:07:56 +0000

    Style header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import './index.css';
 
 export default function Header() {
-  return <div className="text-blue-700">Alice Dashboard</div>;
+  return (
+    <div className="header flex-row-center">
+      <h1>Alice Dashboard</h1>
+    </div>
+  );
 }
