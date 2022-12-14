e68f8336d1d9fbefdb17834128fe0cc01a12a8e6
jihye <jiji.pak41@gmail.com>
2022-12-14 21:22:53 +0000

    Fix typo

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,7 +4,7 @@ import './index.css';
 export default function Header() {
   return (
     <div className="header flex-row-center">
-      <h1>Alice Dashboard</h1>
+      <h1>Alice's Dashboard</h1>
     </div>
   );
 }
